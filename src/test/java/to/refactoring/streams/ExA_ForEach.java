bb970746aa138eeacf29eb9c1bfbf201fd055b00
Nat Pryce <sw@natpryce.com>
2016-07-15 08:50:43 +0100

    Nat - experiment with removing the @Way mechanism
     - it turns out that IntelliJ is very particular about when it offers the 'Convert to forEach' refactoring.  It seems the loop body must contain only a single statement, and the statement must contain only one method call through a local variable.  I introduced the StringPrinter so I don't have to create a StringWriter and PrintWriter in every method

--- src/test/java/to/refactoring/streams/ExA_ForEach.java
+++ src/test/java/to/refactoring/streams/ExA_ForEach.java
@@ -1,39 +1,40 @@
 package to.refactoring.streams;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import to.refactoring.runner.ExampleRunner;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.List;
-import java.util.function.Consumer;
 
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static to.refactoring.runner.ExampleRunner.Way;
 
 
-@RunWith(ExampleRunner.class)
 public class ExA_ForEach {
-
-    @Way
-    public static void indexed_for_loop(List<String> things, PrintWriter writer) {
-        for (int i = 0; i < things.size(); i++) {
-            writer.println(things.get(i));
-        }
-    }
+    List<String> things = asList("one", "two", "three", "four");
 
     @Test
-    public void test(Thing f) {
-        Writer writer = new StringWriter();
-        f.call(asList("one", "two", "three"), new PrintWriter(writer));
-        assertThat(writer.toString(), equalTo("one\ntwo\nthree\n"));
+    public void indexed_for_loop() {
+        StringPrinter joined = new StringPrinter();
+
+        for (String s : things) {
+            joined.println(s);
+        }
+
+        assertThat(joined.toString(), equalTo("one\ntwo\nthree\nfour\n"));
     }
 
-    public interface Thing {
-        void call(Iterable<String> things, PrintWriter writer);
+    static class StringPrinter extends PrintWriter {
+        public StringPrinter() {
+            super(new StringWriter());
+        }
+
+        @Override
+        public String toString() {
+            return out.toString();
+        }
     }
 }
+

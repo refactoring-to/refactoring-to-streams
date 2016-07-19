302e673ec9b3eca6ac1e5a67464fa27f522e2713
Nat Pryce <sw@natpryce.com>
2016-07-19 12:57:58 +0100

    Nat - starting point

--- src/test/java/to/refactoring/streams/ExA_ForEach.java
+++ src/test/java/to/refactoring/streams/ExA_ForEach.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.io.Writer;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -13,28 +12,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class ExA_ForEach {
-    List<String> things = asList("one", "two", "three", "four");
+    final List<String> things = asList("one", "two", "three", "four");
+
+    final StringWriter text = new StringWriter();
+    final PrintWriter printer = new PrintWriter(text);
 
     @Test
     public void indexed_for_loop() {
-        StringPrinter joined = new StringPrinter();
-
-        for (String s : things) {
-            joined.println(s);
+        for (int i = 0; i < things.size(); i++) {
+            String s = things.get(i);
+            printer.println(s.toUpperCase());
         }
 
-        assertThat(joined.toString(), equalTo("one\ntwo\nthree\nfour\n"));
-    }
-
-    static class StringPrinter extends PrintWriter {
-        public StringPrinter() {
-            super(new StringWriter());
-        }
-
-        @Override
-        public String toString() {
-            return out.toString();
-        }
+        assertThat(text.toString(), equalTo("ONE\nTWO\nTHREE\nFOUR\n"));
     }
 }
 

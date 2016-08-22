ace377c1e2b9de870a55a81ea8b856fe0aaae788
Nat Pryce <sw@natpryce.com>
2016-08-22 18:46:46 +0100

    Starting point for Filtering episode

---
+++ src/test/java/to/refactoring/streams/ExC_Filtering.java
@@ -0,0 +1,27 @@
+package to.refactoring.streams;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+public class ExC_Filtering {
+    List<Integer> numbers = asList(1, 2, 3, 4, 5, 6, 7, 8);
+
+    @Test
+    public void filtering_even_numbers() {
+        List<Integer> evenNumbers = new ArrayList<>();
+        for (int x : numbers) {
+            if (x % 2 == 0) {
+                evenNumbers.add(x);
+            }
+        }
+
+        assertThat(evenNumbers, equalTo(asList(2, 4, 6, 8)));
+    }
+}

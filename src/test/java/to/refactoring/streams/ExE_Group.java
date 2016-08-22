21ab343c00c96f603741fa868f8e380b78af3da0
Nat Pryce <sw@natpryce.com>
2016-08-22 21:18:38 +0100

    Make groups a HashMap, to simplify translation to stream pipeline

--- src/test/java/to/refactoring/streams/ExE_Group.java
+++ src/test/java/to/refactoring/streams/ExE_Group.java
@@ -2,19 +2,17 @@ package to.refactoring.streams;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.groupingBy;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ExE_Group {
     List<String> numbers = asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
 
-    Map<Integer, List<String>> expectedGroups = new TreeMap<>();
+    Map<Integer, List<String>> expectedGroups = new HashMap<>();
     {
         expectedGroups.put(3, asList("one", "two", "six", "ten"));
         expectedGroups.put(4, asList("four", "five", "nine"));
@@ -23,7 +21,7 @@ public class ExE_Group {
 
     @Test
     public void for_loop() {
-        Map<Integer, List<String>> groups = new TreeMap<>();
+        Map<Integer, List<String>> groups = new HashMap<>();
 
         for (String number : numbers) {
             int length = number.length();
